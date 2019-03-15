4b9395ad5a12bd8d0710d5466f9b0310f64e7787
Minh Tri Le <minhtri.manchester@gmail.com>
2019-03-15 22:45:24 +1100

    Update Products Container

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,36 +1,16 @@
 import React, {Component} from 'react';
-import Product from "./Product";
-import { connect } from "react-redux";
 
 class Products extends Component {
     render() {
-        var {products} = this.props;
-
         return (
         <section className="section">
             <h1 className="section-heading">Danh Sách Sản Phẩm</h1>
             <div className="row">
-                {this.showProducts(products)}
+                {this.props.children}
             </div>
         </section>
         );
     }
-
-    showProducts(products) {
-        var results = null;
-        if (products.length > 0) {
-            results = products.map((product, index) => {
-                return <Product key={index} product={product}/>
-            });
-        }
-        return results;
-    }
 }
             
-const mapStateToProps = state => {
-    return {
-        products : state.products
-    }
-};
-
-export default connect(mapStateToProps, null)(Products);
+export default Products;
